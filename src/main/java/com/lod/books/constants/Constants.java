6c4a116087df1d53b0d51d96c6ace09fa5d6ea84
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2022-03-25 23:46:14 +0100

    Update Constants.java

--- src/main/java/com/lod/books/constants/Constants.java
+++ src/main/java/com/lod/books/constants/Constants.java
@@ -2,10 +2,8 @@ package com.lod.books.constants;
 
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.regex.Pattern;
 
 /**
  * Utility class that holds values for constants used
@@ -16,28 +14,23 @@ public class Constants {
     public static final String DBPEDIA_SPARQL_URL_GRAPH = "?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=";
     public static final String DBPEDIA_SPARQL_URL_END = "&format=application%2Fsparql-results%2Bjson&timeout=30000&signal_void=on&signal_unconnected=on";
 
+    // books
     public static final String getBookDetailsURL(String searchBookTitle) {
-        String search = "";
-        try {
-            search = URLEncoder.encode(replaceSymbol(searchBookTitle), StandardCharsets.UTF_8.toString());
-        } catch (Exception ex) {
-            ex.getMessage();
-        }
-        String searchText = "dbr%3A" + search;
+        String search = "dbr%3A" + encodeValue(replaceSymbol(searchBookTitle));
         return DBPEDIA_SPARQL_URL + DBPEDIA_SPARQL_URL_GRAPH +
                 "select+distinct+%3Fab+%3Fa+%3FaL+%3Fl+%3Fn+%3FlG+%3Fg+%3Fp+%3FnP+%3Fpr+%3Fpd+%3Ft+%3Flg+where+%7B%0D%0A" +
-                searchText + "+dbo%3Aabstract+%3Fab.%0D%0AOPTIONAL%7B" +
-                searchText + "+dbo%3Aauthor+%3FaL.+%3FaL+dbp%3Aname+%3Fa%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+rdfs%3Alabel+%3Fl%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+foaf%3Aname+%3Fn%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbo%3AliteraryGenre+%3FlG%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbp%3Agenre+%3Fg%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbo%3AnumberOfPages+%3FnP%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbp%3Apages+%3Fp%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbo%3Apublisher+%3Fpe.%3Fpe+dbp%3Aname+%3Fpr%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbp%3Apublished+%3Fpd%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbo%3Athumbnail+%3Ft%7D%0D%0AOPTIONAL%7B+" +
-                searchText + "+dbp%3Alanguage+%3Flg%7D%0D%0AFILTER%28lang%28%3Fab%29%3D%22en%22%29%7DLIMIT+10+" +
+                search + "+dbo%3Aabstract+%3Fab.%0D%0AOPTIONAL%7B" +
+                search + "+dbo%3Aauthor+%3FaL.+%3FaL+dbp%3Aname+%3Fa%7D%0D%0AOPTIONAL%7B+" +
+                search + "+rdfs%3Alabel+%3Fl%7D%0D%0AOPTIONAL%7B+" +
+                search + "+foaf%3Aname+%3Fn%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbo%3AliteraryGenre+%3FlG%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbp%3Agenre+%3Fg%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbo%3AnumberOfPages+%3FnP%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbp%3Apages+%3Fp%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbo%3Apublisher+%3Fpe.%3Fpe+dbp%3Aname+%3Fpr%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbp%3Apublished+%3Fpd%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbo%3Athumbnail+%3Ft%7D%0D%0AOPTIONAL%7B+" +
+                search + "+dbp%3Alanguage+%3Flg%7D%0D%0AFILTER%28lang%28%3Fab%29%3D%22en%22%29%7DLIMIT+10+" +
                 DBPEDIA_SPARQL_URL_END;
     }
 
@@ -53,12 +46,7 @@ public class Constants {
     }
 
     public static final String getBookSearchURL(String searchBookTitle, Integer limit) {
-        String search = "";
-        try {
-            search = URLEncoder.encode(searchBookTitle.toLowerCase(), StandardCharsets.UTF_8.toString());
-        } catch (Exception ex) {
-            ex.getMessage();
-        }
+        String search = encodeValue(searchBookTitle.toLowerCase());
         return DBPEDIA_SPARQL_URL + DBPEDIA_SPARQL_URL_GRAPH +
                 "select+distinct+%3Fbook+%3Flabel+%3Fauthor+%3FnumberOfPages%3Fpages+where+%7B%3Fbook+rdf%3Atype+dbo%3ABook%3Brdfs%3Alabel+%3Flabel.%0D%0A"
                 + "OPTIONAL+%7B+%3Fbook+dbo%3Aauthor+%3FauthorLink.+%3FauthorLink+dbp%3Aname+%3Fauthor+%7D%0D%0A"
@@ -71,17 +59,74 @@ public class Constants {
                 + DBPEDIA_SPARQL_URL_END;
     }
 
-    private String encodeValue(String value) throws UnsupportedEncodingException {
-        return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+    // authors
+    public static final String getAuthorDetailsURL(String searchAuthorName) {
+        return DBPEDIA_SPARQL_URL + DBPEDIA_SPARQL_URL_GRAPH +
+                encodeValue("select ?a ?l ?ab ?bd ?bp ?dd ?dp ?t ?n ?o ?ay (count(?w) as ?nw) ?w where {" +
+                        "?w dbo:author ?a." +
+                        "?a rdfs:label ?l." +
+                        "OPTIONAL { ?a dbo:abstract ?ab}" +
+                        "OPTIONAL { ?a dbo:birthDate ?bd}" +
+                        "OPTIONAL { ?a dbo:birthPlace ?bPL. ?bPL rdfs:label ?bp}" +
+                        "OPTIONAL { ?a dbo:deathDate ?dd}" +
+                        "OPTIONAL { ?a dbp:nationality ?dPL. ?dPL rdfs:label ?dp}" +
+                        "OPTIONAL { ?a dbo:thumbnail ?t}" +
+                        "OPTIONAL { ?a dbp:nationality ?n}" +
+                        "OPTIONAL { ?a dbp:occupation ?o}" +
+                        "OPTIONAL { ?a dbo:activeYearsStartYear ?ay}" +
+                        "FILTER (lang(?l) = \"en\")." +
+                        "}" +
+                        "group by ?a ?l ?ab ?bd ?bp ?dd ?dp ?t ?n ?o ?ay ?w " +
+                        "LIMIT 10") +
+                DBPEDIA_SPARQL_URL_END;
     }
+
+    public static final String getAuthorsURL(Integer limit) {
+        return DBPEDIA_SPARQL_URL + DBPEDIA_SPARQL_URL_GRAPH +
+                encodeValue("select ?author ?label ?occupation (count(?work) as ?nw) where {" +
+                        "?work dbo:author ?author." +
+                        "?author rdfs:label ?label." +
+                        "OPTIONAL { ?author dbp:occupation ?occupation}" +
+                        "FILTER (lang(?label) = \"en\")." +
+                        "}" +
+                        "group by ?author ?label ?occupation " +
+                        "LIMIT " + limit.toString())
+                + DBPEDIA_SPARQL_URL_END;
+    }
+
+    public static final String getAuthorSearchURL(String searchAuthorName, Integer limit) {
+        String search = encodeValue(searchAuthorName.toLowerCase());
+        return DBPEDIA_SPARQL_URL + DBPEDIA_SPARQL_URL_GRAPH +
+                encodeValue("select ?author ?label ?occupation (count(?work) as ?nw) where {" +
+                        "?work dbo:author ?author." +
+                        "?author rdfs:label ?label." +
+                        "OPTIONAL { ?author dbp:occupation ?occupation}" +
+                        "FILTER (lang(?label) = \"en\")." +
+                        "FILTER regex(lcase(str(?label)), \".*" + search + "\")." +
+                        "}" +
+                        "group by ?author ?label ?occupation " +
+                        "LIMIT " + limit.toString())
+                + DBPEDIA_SPARQL_URL_END;
+    }
+
+
+    //private functions
+    private static String encodeValue(String value) {
+        String encodedValue = "";
+        try {
+            encodedValue = URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+        return encodedValue;
+    }
+
     private static String replaceSymbol(String value) {
-
-        value=value.replaceAll(",","\\\\,");
-        value=value.replaceAll("'","\\\\'");
-        value=value.replaceAll("\\(","\\\\(");
-        value=value.replaceAll("\\)","\\\\)");
-        value=value.replaceAll("&","\\\\&");
-
+        value = value.replaceAll(",", "\\\\,");
+        value = value.replaceAll("'", "\\\\'");
+        value = value.replaceAll("\\(", "\\\\(");
+        value = value.replaceAll("\\)", "\\\\)");
+        value = value.replaceAll("&", "\\\\&");
         return value;
     }
 
